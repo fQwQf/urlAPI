2c704c27bc44b60166510f6233cdb3589b7b00a1
Homo Node <i@0x535a.cn>
2026-05-23 12:24:42 +0000

    update action workflows

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-BACKEND_URL="${BACKEND_URL:-http://localhost:2233}"
+BACKEND_URL="${BACKEND_URL:-http://urlapi:2233}"
 
 export BACKEND_URL
 envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
